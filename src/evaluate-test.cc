26a095906ab77b2e86b0b5611f1726b216d123f1
Aditya Saligrama <saligrama427@gmail.com>
2018-02-25 21:55:44 -0500

    initial commit

---
+++ src/evaluate-test.cc
@@ -0,0 +1,15 @@
+#include "evaluate.hh"
+
+NullLogger * logger;
+
+int main(int argc, char ** argv)
+{
+    logger = new NullLogger();
+
+    int r = run_random_tests(1000, 30, 701, 0.15, 0, 2, curious_cmp, "curious", false,
+                             100000, 0.0000001, time(NULL), 3);
+
+    delete logger;
+
+    return r;
+}
